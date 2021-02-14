f33f42d36a3f5ce8bf24c15230c9ca1d73c43f66
jcundill <jcundill@gmail.com>
2021-02-14 13:44:52 +0000

    wire in reading/writing gpx. add both number and description to ControlSite

---
+++ src/main/java/org/streeto/gpx/GpxFacade.java
@@ -0,0 +1,104 @@
+package org.streeto.gpx;
+
+import com.graphhopper.util.shapes.GHPoint;
+import io.jenetics.jpx.GPX;
+import io.jenetics.jpx.Route;
+import io.jenetics.jpx.Track;
+import io.jenetics.jpx.WayPoint;
+import io.jenetics.jpx.geom.Geoid;
+import org.streeto.ControlSite;
+import org.streeto.Course;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.streeto.utils.CollectionHelpers.streamFromPointList;
+
+public class GpxFacade {
+
+    public static Course readCourse(String filename) throws IOException {
+        var is = new FileInputStream(filename);
+        var gpx = GPX.reader(GPX.Reader.Mode.STRICT).read(is);
+        var route = gpx.routes()
+                .map(GpxFacade::toCourseControls)
+                .findFirst()
+                .orElseThrow();
+        var dist = gpx.getTracks().get(0).getSegments().get(0).getPoints().stream().collect(Geoid.DEFAULT.toTourLength()).doubleValue();
+        return new Course(dist, route.size() - 2, route);
+    }
+
+    private static List<ControlSite> toCourseControls(Route route) {
+        return route.points()
+                .map(GpxFacade::toControlSite)
+                .collect(Collectors.toList());
+    }
+
+    private static ControlSite toControlSite(WayPoint wayPoint) {
+        return new ControlSite(wayPoint.getLatitude().doubleValue(),
+                wayPoint.getLongitude().doubleValue(),
+                wayPoint.getName().orElse(""),
+                wayPoint.getDescription().orElse(""));
+    }
+
+    public static void writeCourse(String filename, Course course) throws IOException {
+        var points = streamFromPointList(course.getRoute()
+                .getPoints())
+                .map(GpxFacade::toWayPoint)
+                .collect(Collectors.toList());
+        var controls = course.getControls().stream()
+                .map(GpxFacade::toWayPoint).collect(Collectors.toList());
+        var route = toRoute(controls);
+        var gpxBuilder = GPX.builder()
+                .addTrack(track -> track
+                        .name("Best Track")
+                        .addSegment(s -> s.points(points)))
+                .addRoute(route)
+                .creator("StreetO");
+        controls.forEach(gpxBuilder::addWayPoint);
+        var gpx = gpxBuilder.build();
+
+
+        GPX.writer("    ")
+                .write(gpx, filename);
+
+    }
+
+    private static Route toRoute(List<WayPoint> controls) {
+        var builder = Route.builder();
+        controls.forEach(builder::addPoint);
+        return builder.build();
+    }
+
+    private static List<GHPoint> toGHPoints(Track track) {
+        return track.getSegments().get(0).points()
+                .map(p -> new GHPoint(
+                        p.getLatitude().doubleValue(),
+                        p.getLongitude().doubleValue())
+                ).collect(Collectors.toList());
+    }
+
+    private static WayPoint toWayPoint(GHPoint p) {
+        return WayPoint.builder().lat(p.lat).lon(p.lon).build();
+    }
+
+    private static WayPoint toWayPoint(ControlSite site) {
+        return WayPoint.builder()
+                .lat(site.getLocation().lat)
+                .lon(site.getLocation().lon)
+                .name(site.getNumber())
+                .desc(site.getDescription())
+                .build();
+    }
+
+    public static void main(String[] args) {
+        try {
+            var course = readCourse("/home/jon/code/streeto/abc.gpx");
+            System.out.println(course);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
