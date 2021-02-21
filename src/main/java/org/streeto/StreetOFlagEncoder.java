6b035e45b90cc3cd8fd18ec5791c6612fecf11e8
jcundill <jcundill@gmail.com>
2021-02-21 11:27:45 +0000

    update Graphhopper to 2.3

--- src/main/java/org/streeto/StreetOFlagEncoder.java
+++ src/main/java/org/streeto/StreetOFlagEncoder.java
@@ -1,59 +1,65 @@
 package org.streeto;
 
-import com.graphhopper.reader.ReaderRelation;
 import com.graphhopper.reader.ReaderWay;
-import com.graphhopper.routing.profiles.DecimalEncodedValue;
-import com.graphhopper.routing.profiles.EncodedValue;
-import com.graphhopper.routing.profiles.UnsignedDecimalEncodedValue;
+import com.graphhopper.routing.ev.*;
 import com.graphhopper.routing.util.AbstractFlagEncoder;
-import com.graphhopper.routing.util.EncodedValueOld;
 import com.graphhopper.routing.util.EncodingManager;
 import com.graphhopper.routing.util.PriorityCode;
+import com.graphhopper.routing.util.spatialrules.TransportationMode;
 import com.graphhopper.routing.weighting.PriorityWeighting;
 import com.graphhopper.storage.IntsRef;
 import com.graphhopper.util.PMap;
 
 import java.util.*;
 
-public class StreetOFlagEncoder extends AbstractFlagEncoder {
+import static com.graphhopper.routing.util.EncodingManager.getKey;
+import static com.graphhopper.routing.util.PriorityCode.*;
+import static com.graphhopper.routing.util.spatialrules.TransportationMode.FOOT;
 
-    protected final HashSet<String> sidewalkValues;
-    protected final HashSet<String> sidewalksNoValues;
-    final Set<String> safeHighwayTags;
-    final Set<String> allowedHighwayTags;
-    final Set<String> avoidHighwayTags;
-    final Map<String, Integer> hikingNetworkToCode;
+/**
+ * Defines bit layout for pedestrians (speed, access, surface, ...). Here we put a penalty on unsafe
+ * roads only. If you wish to also prefer routes due to beauty like hiking routes use the
+ * HikeFlagEncoder instead.
+ * <p>
+ *
+ * @author Peter Karich
+ * @author Nop
+ * @author Karl Hübner
+ */
+public class StreetOFlagEncoder extends AbstractFlagEncoder {
+    static final int SLOW_SPEED = 2;
+    static final int MEAN_SPEED = 5;
+    static final int FERRY_SPEED = 15;
+    final Set<String> safeHighwayTags = new HashSet<>();
+    final Set<String> allowedHighwayTags = new HashSet<>();
+    final Set<String> avoidHighwayTags = new HashSet<>();
+    final Set<String> allowedSacScale = new HashSet<>();
+    protected final HashSet<String> sidewalkValues = new HashSet<>(5);
+    protected final HashSet<String> sidewalksNoValues = new HashSet<>(5);
+    protected boolean speedTwoDirections = false;
     private DecimalEncodedValue priorityWayEncoder;
-    private EncodedValueOld relationCodeEncoder;
+
     public StreetOFlagEncoder() {
-        this(4, 1.0D);
+        this(4, 1);
     }
 
     public StreetOFlagEncoder(PMap properties) {
-        this((int) properties.getLong("speedBits", 4L), properties.getDouble("speedFactor", 1.0D));
-        this.properties = properties;
-        this.setBlockFords(properties.getBool("block_fords", true));
+        this(properties.getInt("speed_bits", 4), properties.getDouble("speed_factor", 1));
+
+        blockPrivate(true);
+        blockFords(false);
+        blockBarriersByDefault(false);
     }
 
-    public StreetOFlagEncoder(String propertiesStr) {
-        this(new PMap(propertiesStr));
-    }
-
-    public StreetOFlagEncoder(int speedBits, double speedFactor) {
+    protected StreetOFlagEncoder(int speedBits, double speedFactor) {
         super(speedBits, speedFactor, 0);
-        this.safeHighwayTags = new HashSet<>();
-        this.allowedHighwayTags = new HashSet<>();
-        this.avoidHighwayTags = new HashSet<>();
-        this.hikingNetworkToCode = new HashMap<>();
-        this.sidewalkValues = new HashSet<>(5);
-        this.sidewalksNoValues = new HashSet<>(5);
+        restrictions.addAll(Arrays.asList("foot", "access"));
 
-        restrictions.addAll(List.of("foot", "access"));
-        restrictedValues.add("private");
         restrictedValues.add("no");
         restrictedValues.add("restricted");
         restrictedValues.add("military");
         restrictedValues.add("emergency");
+        restrictedValues.add("private");
 
         intendedValues.add("yes");
         intendedValues.add("designated");
@@ -64,15 +70,16 @@ public class StreetOFlagEncoder extends AbstractFlagEncoder {
         sidewalksNoValues.add("none");
         // see #712
         sidewalksNoValues.add("separate");
+
         sidewalkValues.add("yes");
         sidewalkValues.add("both");
         sidewalkValues.add("left");
         sidewalkValues.add("right");
 
-        setBlockByDefault(false);
+        blockBarriersByDefault(false);
         absoluteBarriers.add("fence");
-//        potentialBarriers.add("gate")
-//        potentialBarriers.add("cattle_grid")
+        potentialBarriers.add("gate");
+        potentialBarriers.add("cattle_grid");
 
         safeHighwayTags.add("footway");
         safeHighwayTags.add("path");
@@ -81,13 +88,13 @@ public class StreetOFlagEncoder extends AbstractFlagEncoder {
         safeHighwayTags.add("living_street");
         safeHighwayTags.add("track");
         safeHighwayTags.add("residential");
+        safeHighwayTags.add("service");
         safeHighwayTags.add("platform");
-        safeHighwayTags.add("cycleway");
-        safeHighwayTags.add("unclassified");
-        safeHighwayTags.add("road");
-        // disallowed in some countries
-        safeHighwayTags.add("bridleway");
 
+        avoidHighwayTags.add("trunk");
+        avoidHighwayTags.add("trunk_link");
+        avoidHighwayTags.add("primary");
+        avoidHighwayTags.add("primary_link");
         avoidHighwayTags.add("secondary");
         avoidHighwayTags.add("secondary_link");
         avoidHighwayTags.add("tertiary");
@@ -97,164 +104,152 @@ public class StreetOFlagEncoder extends AbstractFlagEncoder {
         //avoidHighwayTags.add("cycleway");
         allowedHighwayTags.addAll(safeHighwayTags);
         allowedHighwayTags.addAll(avoidHighwayTags);
+        allowedHighwayTags.add("cycleway");
+        allowedHighwayTags.add("unclassified");
+        allowedHighwayTags.add("road");
+        // disallowed in some countries
+        allowedHighwayTags.add("bridleway");
 
+        allowedSacScale.add("hiking");
+        allowedSacScale.add("mountain_hiking");
+        allowedSacScale.add("demanding_mountain_hiking");
 
-        this.hikingNetworkToCode.put("iwn", PriorityCode.UNCHANGED.getValue());
-        this.hikingNetworkToCode.put("nwn", PriorityCode.UNCHANGED.getValue());
-        this.hikingNetworkToCode.put("rwn", PriorityCode.UNCHANGED.getValue());
-        this.hikingNetworkToCode.put("lwn", PriorityCode.UNCHANGED.getValue());
-        this.maxPossibleSpeed = 15;
-        this.speedDefault = 10.0D;
-        this.init();
+        maxPossibleSpeed = FERRY_SPEED;
+        speedDefault = MEAN_SPEED;
     }
 
     @Override
-    public String toString() {
-        return "streeto";
-    }
-
     public int getVersion() {
         return 5;
     }
 
+    @Override
+    public TransportationMode getTransportationMode() {
+        return FOOT;
+    }
+
+    @Override
     public void createEncodedValues(List<EncodedValue> registerNewEncodedValue, String prefix, int index) {
+        // first two bits are reserved for route handling in superclass
         super.createEncodedValues(registerNewEncodedValue, prefix, index);
-        registerNewEncodedValue.add(this.speedEncoder = new UnsignedDecimalEncodedValue(EncodingManager.getKey(prefix, "average_speed"), this.speedBits, this.speedFactor, false));
-        registerNewEncodedValue.add(this.priorityWayEncoder = new UnsignedDecimalEncodedValue(EncodingManager.getKey(prefix, "priority"), 3, PriorityCode.getFactor(1), false));
+        // larger value required - ferries are faster than pedestrians
+        registerNewEncodedValue.add(avgSpeedEnc = new UnsignedDecimalEncodedValue(getKey(prefix, "average_speed"), speedBits, speedFactor, speedTwoDirections));
+        registerNewEncodedValue.add(priorityWayEncoder = new UnsignedDecimalEncodedValue(getKey(prefix, "priority"), 3, PriorityCode.getFactor(1), speedTwoDirections));
+
+        //footRouteEnc = getEnumEncodedValue(RouteNetwork.key("foot"), RouteNetwork.class);
     }
 
-    public int defineRelationBits(int index, int shift) {
-        this.relationCodeEncoder = new EncodedValueOld("RelationCode", shift, 3, 1.0D, 0L, 7);
-        return shift + this.relationCodeEncoder.getBits();
-    }
-
-    public long handleRelationTags(long oldRelationFlags, ReaderRelation relation) {
-        int code = 0;
-        if (!relation.hasTag("route", "hiking") && !relation.hasTag("route", "foot")) {
-            if (relation.hasTag("route", "ferry")) {
-                code = PriorityCode.AVOID_IF_POSSIBLE.getValue();
-            }
-        } else {
-            Integer val = this.hikingNetworkToCode.get(relation.getTag("network"));
-            if (val != null) {
-                code = val;
-            } else {
-                code = this.hikingNetworkToCode.get("lwn");
-            }
-        }
-
-        int oldCode = (int) this.relationCodeEncoder.getValue(oldRelationFlags);
-        return oldCode < code ? this.relationCodeEncoder.setValue(0L, code) : oldRelationFlags;
-    }
-
+    /**
+     * Some ways are okay but not separate for pedestrians.
+     */
+    @Override
     public EncodingManager.Access getAccess(ReaderWay way) {
         String highwayValue = way.getTag("highway");
         if (highwayValue == null) {
             EncodingManager.Access acceptPotentially = EncodingManager.Access.CAN_SKIP;
-            if (way.hasTag("route", this.ferries)) {
-                String footTag = way.getTag("foot");
-                if (footTag == null || this.intendedValues.contains(footTag)) {
-                    acceptPotentially = EncodingManager.Access.FERRY;
-                }
+
+            if (way.hasTag("route", ferries)) {
+                return EncodingManager.Access.CAN_SKIP; // haven't got time to get a ferry
             }
 
-            if (way.hasTag("railway", "platform")) {
+            // special case not for all acceptedRailways, only platform
+            if (way.hasTag("railway", "platform"))
                 acceptPotentially = EncodingManager.Access.WAY;
-            }
 
-            if (way.hasTag("man_made", "pier")) {
+            if (way.hasTag("man_made", "pier"))
                 acceptPotentially = EncodingManager.Access.WAY;
-            }
 
             if (!acceptPotentially.canSkip()) {
-                return way.hasTag(this.restrictions, this.restrictedValues) && !this.getConditionalTagInspector().isRestrictedWayConditionallyPermitted(way) ? EncodingManager.Access.CAN_SKIP : acceptPotentially;
-            } else {
-                return EncodingManager.Access.CAN_SKIP;
+                if (way.hasTag(restrictions, restrictedValues) && !getConditionalTagInspector().isRestrictedWayConditionallyPermitted(way))
+                    return EncodingManager.Access.CAN_SKIP;
+                return acceptPotentially;
             }
-        } else {
-            String sacScale = way.getTag("sac_scale");
-            if (sacScale != null && !"hiking".equals(sacScale) && !"mountain_hiking".equals(sacScale) && !"demanding_mountain_hiking".equals(sacScale) && !"alpine_hiking".equals(sacScale)) {
-                return EncodingManager.Access.CAN_SKIP;
-            } else if (way.hasTag("foot", this.intendedValues)) {
-                return EncodingManager.Access.WAY;
-            } else if (way.hasTag(this.restrictions, this.restrictedValues) && !this.getConditionalTagInspector().isRestrictedWayConditionallyPermitted(way)) {
-                return EncodingManager.Access.CAN_SKIP;
-            } else if (way.hasTag("sidewalk", this.sidewalkValues)) {
-                return EncodingManager.Access.WAY;
-            } else if (!this.allowedHighwayTags.contains(highwayValue)) {
-                return EncodingManager.Access.CAN_SKIP;
-            } else if (way.hasTag("motorroad", "yes")) {
-                return EncodingManager.Access.CAN_SKIP;
-            } else if (!this.isBlockFords() || !way.hasTag("highway", "ford") && !way.hasTag("ford")) {
-                return this.getConditionalTagInspector().isPermittedWayConditionallyRestricted(way) ? EncodingManager.Access.CAN_SKIP : EncodingManager.Access.WAY;
-            } else {
-                return EncodingManager.Access.CAN_SKIP;
-            }
+
+            return EncodingManager.Access.CAN_SKIP;
         }
+
+        // commented out, will run up anything
+        // other scales are too dangerous, see http://wiki.openstreetmap.org/wiki/Key:sac_scale
+//        if (way.getTag("sac_scale") != null && !way.hasTag("sac_scale", allowedSacScale))
+//            return EncodingManager.Access.CAN_SKIP;
+
+        // no need to evaluate ferries or fords - already included here
+        if (way.hasTag("foot", intendedValues))
+            return EncodingManager.Access.WAY;
+
+        // check access restrictions
+        if (way.hasTag(restrictions, restrictedValues) && !getConditionalTagInspector().isRestrictedWayConditionallyPermitted(way))
+            return EncodingManager.Access.CAN_SKIP;
+
+        if (way.hasTag("sidewalk", sidewalkValues))
+            return EncodingManager.Access.WAY;
+
+        if (!allowedHighwayTags.contains(highwayValue))
+            return EncodingManager.Access.CAN_SKIP;
+
+        if (way.hasTag("motorroad", "yes"))
+            return EncodingManager.Access.CAN_SKIP;
+
+        // do not get our feet wet, "yes" is already included above
+        if (way.hasTag("highway", "ford") || way.hasTag("ford"))
+            return EncodingManager.Access.WAY;
+
+        if (getConditionalTagInspector().isPermittedWayConditionallyRestricted(way))
+            return EncodingManager.Access.CAN_SKIP;
+
+        return EncodingManager.Access.WAY;
     }
 
-    public IntsRef handleWayTags(IntsRef edgeFlags, ReaderWay way, EncodingManager.Access access, long relationFlags) {
-        if (!access.canSkip()) {
-            if (!access.isFerry()) {
-                String sacScale = way.getTag("sac_scale");
-                if (sacScale != null) {
-                    if ("hiking".equals(sacScale)) {
-                        this.speedEncoder.setDecimal(false, edgeFlags, 5.0D);
-                    } else {
-                        this.speedEncoder.setDecimal(false, edgeFlags, 2.0D);
-                    }
-                } else {
-                    this.speedEncoder.setDecimal(false, edgeFlags, 5.0D);
-                }
+    @Override
+    public IntsRef handleWayTags(IntsRef edgeFlags, ReaderWay way, EncodingManager.Access access) {
+        if (access.canSkip())
+            return edgeFlags;
 
+        int priorityFromRelation = UNCHANGED.getValue();
+        accessEnc.setBool(false, edgeFlags, true);
+        accessEnc.setBool(true, edgeFlags, true);
+        if (!access.isFerry()) {
+            String sacScale = way.getTag("sac_scale");
+            if (sacScale != null) {
+                setSpeed(edgeFlags, "hiking".equals(sacScale) ? MEAN_SPEED : SLOW_SPEED);
             } else {
-                double ferrySpeed = this.getFerrySpeed(way);
-                this.setSpeed(false, edgeFlags, ferrySpeed);
+                setSpeed(edgeFlags, way.hasTag("highway", "steps") ? MEAN_SPEED - 2 : MEAN_SPEED);
             }
-            this.accessEnc.setBool(false, edgeFlags, true);
-            this.accessEnc.setBool(true, edgeFlags, true);
-
-            int priorityFromRelation = 0;
-            if (relationFlags != 0L) {
-                priorityFromRelation = (int) this.relationCodeEncoder.getValue(relationFlags);
-            }
-
-            this.priorityWayEncoder.setDecimal(false, edgeFlags, PriorityCode.getFactor(this.handlePriority(way, priorityFromRelation)));
+        } else {
+            priorityFromRelation = PriorityCode.AVOID_IF_POSSIBLE.getValue();
+            double ferrySpeed = getFerrySpeed(way);
+            setSpeed(edgeFlags, ferrySpeed);
         }
+
+        priorityWayEncoder.setDecimal(false, edgeFlags, PriorityCode.getFactor(handlePriority(way, priorityFromRelation)));
         return edgeFlags;
     }
 
-    public int defineTurnBits(int index, int shift) {
-        return shift;
+    void setSpeed(IntsRef edgeFlags, double speed) {
+        if (speed > getMaxSpeed())
+            speed = getMaxSpeed();
+        avgSpeedEnc.setDecimal(false, edgeFlags, speed);
+        if (speedTwoDirections)
+            avgSpeedEnc.setDecimal(true, edgeFlags, speed);
     }
 
-    public boolean isTurnRestricted(long flags) {
-        return false;
-    }
-
-    public double getTurnCost(long flag) {
-        return 0.0D;
-    }
-
-    public long getTurnFlags(boolean restricted, double costs) {
-        return 0L;
-    }
-
-    public boolean supports(Class<?> feature) {
-        return super.supports(feature) || PriorityWeighting.class.isAssignableFrom(feature);
-    }
-
-    protected int handlePriority(ReaderWay way, int priorityFromRelation) {
+    int handlePriority(ReaderWay way, Integer priorityFromRelation) {
         TreeMap<Double, Integer> weightToPrioMap = new TreeMap<>();
-        if (priorityFromRelation == 0) {
-            weightToPrioMap.put(0.0D, PriorityCode.UNCHANGED.getValue());
-        } else {
-            weightToPrioMap.put(110.0D, priorityFromRelation);
-        }
+        if (priorityFromRelation == null)
+            weightToPrioMap.put(0d, UNCHANGED.getValue());
+        else
+            weightToPrioMap.put(110d, priorityFromRelation);
 
-        this.collect(way, weightToPrioMap);
+        collect(way, weightToPrioMap);
+
+        // pick priority with biggest order value
         return weightToPrioMap.lastEntry().getValue();
     }
+
+    /**
+     * @param weightToPrioMap associate a weight with every priority. This sorted map allows
+     *                        subclasses to 'insert' more important priorities as well as overwrite determined priorities.
+     */
 
     void collect(ReaderWay way, TreeMap<Double, Integer> weightToPrioMap) {
         String highway = way.getTag("highway");
@@ -267,7 +262,6 @@ public class StreetOFlagEncoder extends AbstractFlagEncoder {
 
         double maxSpeed = this.getMaxSpeed(way);
         if (this.safeHighwayTags.contains(highway) || maxSpeed > 0.0D && maxSpeed <= 20.0D) {
-            weightToPrioMap.put(40.0D, PriorityCode.PREFER.getValue());
             if (way.hasTag("tunnel", this.intendedValues)) {
                 if (way.hasTag("sidewalk", this.sidewalksNoValues)) {
                     weightToPrioMap.put(40.0D, PriorityCode.AVOID_AT_ALL_COSTS.getValue());
@@ -280,4 +274,17 @@ public class StreetOFlagEncoder extends AbstractFlagEncoder {
         }
     }
 
+
+    @Override
+    public boolean supports(Class<?> feature) {
+        if (super.supports(feature))
+            return true;
+
+        return PriorityWeighting.class.isAssignableFrom(feature);
+    }
+
+    @Override
+    public String toString() {
+        return "streeto";
+    }
 }
